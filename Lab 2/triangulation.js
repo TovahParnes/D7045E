2419accadc818731a6ff3a4a8328333f2098b644
TovahParnes <tovah@parnes.com>
2024-11-14 16:41:07 +0100

    removed old code

--- Lab 2/triangulation.js
+++ Lab 2/triangulation.js
@@ -1,22 +1,3 @@
-/*
-function triangulate(points, triangles) {
-	//let triangles = new Float32Array(3 * (points.length - 2)); // Allocate space for each triangle (3 vertices per triangle)
-
-	let tIndex = 0; // Track index in triangles array
-	for (let i = 5; i < points.length; i = i + 2) {
-		// Loop through points to create triangles
-		triangles[tIndex++] = points[i - 5];
-		triangles[tIndex++] = points[i - 4];
-		triangles[tIndex++] = points[i - 3];
-		triangles[tIndex++] = points[i - 2];
-		triangles[tIndex++] = points[i - 1];
-		triangles[tIndex++] = points[i];
-	}
-
-	return triangles;
-}
-*/
-
 function triangulate(points) {
 	const triangleCount = points.length / 2 - 2; // Number of triangles we can form
 	const triangles = new Float32Array(triangleCount * 6); // Each triangle has 3 points (6 coordinates)
