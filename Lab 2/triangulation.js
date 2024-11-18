29d2e2c05d2648685943ca39d039ee8e47bba0b3
TovahParnes <tovah@parnes.com>
2024-11-18 17:24:38 +0100

    working on distance coloring, currently displays a gray color, just not the correct one

--- Lab 2/triangulation.js
+++ Lab 2/triangulation.js
@@ -1,23 +1,3 @@
-function triangulateWrong(points) {
-	const triangleCount = points.length / 2 - 2; // Number of triangles we can form
-	const triangles = new Float32Array(triangleCount * 6); // Each triangle has 3 points (6 coordinates)
-
-	let tIndex = 0; // Track index in triangles array
-	for (let i = 0; i < triangleCount; i++) {
-		// Fill triangles in the order: (1, 2, 3), (2, 3, 4), (3, 4, 5), ...
-		triangles[tIndex++] = points[2 * i];
-		triangles[tIndex++] = points[2 * i + 1];
-
-		triangles[tIndex++] = points[2 * (i + 1)];
-		triangles[tIndex++] = points[2 * (i + 1) + 1];
-
-		triangles[tIndex++] = points[2 * (i + 2)];
-		triangles[tIndex++] = points[2 * (i + 2) + 1];
-	}
-
-	return triangles;
-}
-
 function triangulate(points) {
 	const triangleCount = points.length - 5; // Number of triangles we can form
 	const triangles = new Float32Array(triangleCount * 6); // Each triangle has 3 points (6 coordinates)
