06f345349b78539f667d588965b3f38f4e402adf
TovahParnes <tovah@parnes.com>
2024-12-18 14:28:29 +0100

    updated shader and shader program

--- Temp/ShaderProgram.js
+++ Temp/ShaderProgram.js
@@ -1,24 +1,25 @@
 // Andreas Form och Marcus Asplund
 
-class ShaderProgram{
-    
-    constructor(gl, fragmentShader, vertexShader){
-        this.gl = gl;
-        this.program = gl.createProgram();
-        gl.attachShader(this.program, fragmentShader.getShader());
-        gl.attachShader(this.program, vertexShader.getShader());
-        gl.linkProgram(this.program);
-        if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
-            const info = gl.getProgramInfoLog(this.program);
-            throw new Error(`Could not compile WebGL program. \n\n${info}`);
-        }
-    }
+class ShaderProgram {
+	constructor(gl, vertexShader, fragmentShader) {
+		this.gl = gl;
+		this.program = gl.createProgram();
+		this.vertexShader = vertexShader;
+		this.fragmentShader = fragmentShader;
+		this.gl.attachShader(this.program, this.fragmentShader.getShader());
+		this.gl.attachShader(this.program, this.vertexShader.getShader());
+		gl.linkProgram(this.program);
+		if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+			const info = gl.getProgramInfoLog(this.program);
+			throw new Error(`Could not compile WebGL program. \n\n${info}`);
+		}
+	}
 
-    activate(){
-        this.gl.useProgram(this.program);
-    }
-    
-    getProgram(){
-        return this.program;
-    }
+	activate() {
+		this.gl.useProgram(this.program);
+	}
+
+	getProgram() {
+		return this.program;
+	}
 }
