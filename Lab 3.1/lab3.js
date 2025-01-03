68b18896626dd50301b514ad4c2e27aa070aa784
TovahParnes <tovah@parnes.com>
2025-01-03 10:59:11 +0100

    tried fixing only part of shapes get rendered over a certain num vertexes, seems like there's a max around 1000?

--- Lab 3.1/lab3.js
+++ Lab 3.1/lab3.js
@@ -36,7 +36,7 @@ function init() {
 
 	// Shapes
 	//let cube = new cuboid(gl, 0.8, 0.5, 0.5, shaderProgram);
-	//let sphere = new Sphere(gl, 0.5, 2, 2, shaderProgram);
+	//let sphere = new Sphere(gl, 0.5, 16, 8, shaderProgram);
 	//let star = new Star(gl, 5, 0.3, 0.2, 0.1, shaderProgram);
 	let torus = new Torus(gl, 0.25, 0.5, 15, shaderProgram);
 	//let cone = new Cone(gl, 0.5, 1, 3, shaderProgram);
